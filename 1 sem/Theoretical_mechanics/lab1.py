6f4ac6407c2be9ae035350c345a0f57e382abe46
Maxsmile123 <sysoevmaksim27@gmail.com>
2021-09-13 00:41:55 +0300

    Big changes. release version

--- 1 sem/Theoretical_mechanics/lab1.py
+++ 1 sem/Theoretical_mechanics/lab1.py
@@ -12,8 +12,7 @@ def Rot2D(X, Y, Alpha):
     return RX, RY
 
 t = sp.Symbol('t')
-R = 4
-Omega = 2
+phi = t
 
 
 x = sp.cos(3*t)*sp.cos(t)
@@ -36,7 +35,7 @@ Wmod = sp.sqrt(Wx*Wx+Wy*Wy);
 Wtau = sp.diff(Vmod,t)
 #this is the value of rho but in the picture you should draw the radius, don' t forget!
 rho = (Vmod*Vmod)/sp.sqrt(Wmod*Wmod-Wtau*Wtau)
-xC = R*Omega*t
+
 
 #constructing corresponding arrays
 T = np.linspace(0, 10, 1000)
@@ -45,23 +44,22 @@ X = np.zeros_like(T)
 Y = np.zeros_like(T)
 VX = np.zeros_like(T)
 VY = np.zeros_like(T)
-XC = np.zeros_like(T)
-YC = R
+Rho = np.zeros_like(T)
+Phi = np.zeros_like(T)
 #filling arrays with corresponding values
 for i in np.arange(len(T)):
     X[i] = sp.Subs(x, t, T[i])
     Y[i] = sp.Subs(y, t, T[i])
     VX[i] = sp.Subs(Vx, t, T[i])
     VY[i] = sp.Subs(Vy, t, T[i])
-    XC[i] = sp.Subs(xC, t, T[i])
+    Rho[i] = sp.Subs(Vx, t, T[i])
+    Phi[i] = sp.Subs(Vy, t, T[i])
 
 #here we start to plot
 fig = plt.figure()
 
 ax1 = fig.add_subplot(1, 1, 1)
 ax1.axis('equal')
-ax1.set_xlim([-2, 2])
-ax1.set_ylim([-1, 1])
 
 #plotting a trajectory
 ax1.plot(X, Y)
@@ -74,7 +72,8 @@ ax1.plot([X.min(), X.max()], [0, 0], 'black')
 #of the point A on the disc
 P, = ax1.plot(X[0], Y[0], marker='o')
 #of the velocity vector of this point (line)
-VLine, = ax1.plot([X[0], X[0]+VX[0]], [Y[0], Y[0]+VY[0]], 'r')
+VLine, = ax1.plot([X[0], X[0]+VX[0]], [Y[0], Y[0]+VY[0]], 'g')
+R=math.sqrt(math.pow(X[0],2)+math.pow(Y[0],2))
 
 #of the velocity vector of this point (arrow)
 ArrowX = np.array([-0.2*R, 0, -0.2*R])
@@ -82,15 +81,13 @@ ArrowY = np.array([0.1*R, 0, -0.1*R])
 RArrowX, RArrowY = Rot2D(ArrowX, ArrowY, math.atan2(VY[0], VX[0]))
 VArrow, = ax1.plot(RArrowX + X[0]+VX[0], RArrowY + Y[0]+VY[0])
 
-
 #function for recounting the positions
 def anima(i):
     P.set_data(X[i], Y[i])
-    VLine.set_data([X[i], X[i]+VX[i]], [Y[i], Y[i]+VY[i]])
+    VLine.set_data([X[i], X[i] + VX[i]], [Y[i], Y[i] + VY[i]])
     RArrowX, RArrowY = Rot2D(ArrowX, ArrowY, math.atan2(VY[i], VX[i]))
-    VArrow.set_data(RArrowX + X[i]+VX[i], RArrowY + Y[i]+VY[i])
-
-    return P, VLine, VArrow
+    VArrow.set_data(RArrowX + X[i] + VX[i], RArrowY + Y[i] + VY[i])
+    return P, VLine, VArrow,
 # animation function
 anim = FuncAnimation(fig, anima, frames=1000, interval=2, blit=True)
 
