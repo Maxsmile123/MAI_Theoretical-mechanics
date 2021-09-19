824e035f982b851b421b3f54255534f7133455b3
Maxsmile123 <sysoevmaksim27@gmail.com>
2021-09-20 01:15:21 +0300

    small changes. release version

--- Theoretical_mechanics/lab1.py
+++ Theoretical_mechanics/lab1.py
@@ -6,6 +6,7 @@ import math
 
 # Var 25: r(t) = cos(3t), phi(t) = t
 
+# Функция поворота на угол альфа
 def Rot2D(X, Y, Alpha):
     RX = X*np.cos(Alpha) - Y*np.sin(Alpha)
     RY = X*np.sin(Alpha) + Y*np.cos(Alpha)
@@ -13,33 +14,39 @@ def Rot2D(X, Y, Alpha):
 
 t = sp.Symbol('t')
 phi = t
-
+# Переход их полярных координат в Декартовы координаты
 x = sp.cos(3*t)*sp.cos(phi)
 y = sp.cos(3*t)*sp.sin(phi)
 
+# Вычисление скорости по x
 Vx = sp.diff(x, t)
 print(Vx)
-
+# Вычисление скорости по y
 Vy = sp.diff(y, t)
 print(Vy)
 
-
+# Вычисление общей скорости
 Vmod = sp.sqrt(Vx*Vx+Vy*Vy)
+
+# Вычисление ускорения по x
 Wx = sp.diff(Vx, t)
 print(Wx)
 
+# Вычисление ускорения по y
 Wy = sp.diff(Vy, t)
 print(Wy)
 
+# Вычисление общего ускорения
 Wmod = sp.sqrt(Wx*Wx+Wy*Wy)
-#and here really we could escape integrating, just don't forget that it's absolute value of V here we should differentiate
+# Вычисление тангенсального ускорения
 Wtau = sp.diff(Vmod, t)
-#this is the value of rho but in the picture you should draw the radius, don' t forget!
+# Вычесление радиуса кривизны
 rho = (Vmod*Vmod)/sp.sqrt(Wmod*Wmod-Wtau*Wtau)
 
 
-#constructing corresponding arrays
+# Равномерное распределение по массиву тысячи чисел от 0 до 10.
 T = np.linspace(0, 10, 1000)
+# Заполнение массивов нулями, в соответствии с массивом T
 X = np.zeros_like(T)
 Y = np.zeros_like(T)
 VX = np.zeros_like(T)
@@ -48,7 +55,7 @@ WY = np.zeros_like(T)
 WX = np.zeros_like(T)
 Rho = np.zeros_like(T)
 Phi = np.zeros_like(T)
-#filling arrays with corresponding values
+# Меняем переменную x на t, а t меняем на значение из T[i]. По сути, подстановка значения в символьную переменную
 for i in np.arange(len(T)):
     X[i] = sp.Subs(x, t, T[i])
     Y[i] = sp.Subs(y, t, T[i])
@@ -59,26 +66,21 @@ for i in np.arange(len(T)):
     Rho[i] = sp.Subs(rho, t, T[i])
     Phi[i] = sp.Subs(phi, t, T[i])
 
-#here we start to plot
+# Создать окно
 fig = plt.figure()
-
+# ax1 - окно с графиком
 ax1 = fig.add_subplot(1, 1, 1)
 ax1.axis('equal')
 ax1.set_title("Модель движения точки")
-ax1.set_xlabel('ось абцис')
+ax1.set_xlabel('ось абцисс')
 ax1.set_ylabel('ось ординат')
 
-#plotting a trajectory
+# Построение траектории
 ax1.plot(X, Y)
 
-#plotting a plane for a disc
-# ax1.plot([X.min(), X.max()], [0, 0], 'black')
-
-#plotting initial positions
-
-#of the point A on the disc
+# Построение точки
 P, = ax1.plot(X[0], Y[0], marker='o')
-#of the velocity vector of this point (line)
+# Построение векторов ускорения, скорости, радиуса-кривезны, радиуса-вектора
 WLine, = ax1.plot([X[0], X[0]+WX[0]], [Y[0], Y[0]+WY[0]], 'g', label = 'Вектор ускорения')
 VLine, = ax1.plot([X[0], X[0]+VX[0]], [Y[0], Y[0]+VY[0]], 'r', label = 'Вектор скорости')
 Rholine, = ax1.plot([X[0], X[0] + (Y[0] + VY[0]) * Rho[0] / sp.sqrt((Y[0] + VY[0])**2 +
@@ -88,7 +90,7 @@ sp.sqrt((Y[0] + VY[0])**2 + (X[0] + VX[0])**2)], 'b', label = 'Вектор кр
 RLine, = ax1.plot([0, X[0]], [0, Y[0]], 'black', label = 'Радиус-вектор')
 R = math.sqrt(math.pow(X[0], 2) + math.pow(Y[0], 2))
 
-#of the velocity vector of this point (arrow)
+# Построение стрелочек на концах векторах
 ArrowX = np.array([-0.2*R, 0, -0.2*R])
 ArrowY = np.array([0.1*R, 0, -0.1*R])
 RArrowX, RArrowY = Rot2D(ArrowX, ArrowY, math.atan2(VY[0], VX[0]))
@@ -110,7 +112,7 @@ ux =  Rho[0]*(Y[0] + VY[0])/math.sqrt(math.pow(X[0] + VX[0], 2)+math.pow(Y[0] + 
 uy =  Rho[0]*(X[0] + VX[0])/math.sqrt(np.power(X[0] + VX[0], 2)+math.pow(Y[0] + VY[0], 2))
 RArrowRhox, RArrowRhoy = Rot2D(ArrowRhoX, ArrowRhoY, math.atan2(-uy, ux))
 ArrowRho, = ax1.plot(RArrowRhox + X[0] + ux, RArrowRhoy + Y[0] - uy, 'b')
-
+# Вывод легенды на график
 ax1.legend(
         ncol = 2,    #  количество столбцов
           facecolor = 'oldlace',    #  цвет области
@@ -118,23 +120,27 @@ ax1.legend(
          )
 
 
-#function for recounting the positions
+# Функция для анимации
 def anima(i):
     ux = Rho[i] * (Y[i] + VY[i]) / math.sqrt(math.pow(X[i] + VX[i], 2) + math.pow(Y[i] + VY[i], 2))
     uy = Rho[i] * (X[i] + VX[i]) / math.sqrt(np.power(X[i] + VX[i], 2) + math.pow(Y[i] + VY[i], 2))
+
     P.set_data(X[i], Y[i])
     VLine.set_data([X[i], X[i]+VX[i]], [Y[i], Y[i]+VY[i]])
     Rholine.set_data([X[i], X[i] + ux], [Y[i], Y[i] - uy])
     WLine.set_data([X[i],X[i]+WX[i]],[Y[i],Y[i]+WY[i]])
     RLine.set_data([0, X[i]], [0, Y[i]])
+
     RArrowX, RArrowY = Rot2D(ArrowX, ArrowY, math.atan2(VY[i], VX[i]))
     RWArrowX, RWArrowY = Rot2D(WArrowX, WArrowY, math.atan2(WY[i], WX[i]))
     RArrowRx, RArrowRy = Rot2D(ArrowRx, ArrowRy, math.atan2(Y[i], X[i]))
     RArrowRhox, RArrowRhoy = Rot2D(ArrowRhoX, ArrowRhoY, math.atan2(-uy, ux))
+
     ArrowRho.set_data(RArrowRhox + X[i] + ux, RArrowRhoy + Y[i] - uy)
     VArrow.set_data(RArrowX + X[i]+VX[i], RArrowY + Y[i]+VY[i])
     WArrow.set_data(RWArrowX+X[i]+WX[i], RWArrowY+Y[i]+WY[i])
     RArrow.set_data(RArrowRx + X[i], RArrowRy + Y[i])
+
     return P, VLine, Rholine, VArrow, WLine, WArrow, RLine, RArrow, ArrowRho,
 # animation function
 anim = FuncAnimation(fig, anima, frames=1000, interval=2, blit=True)
