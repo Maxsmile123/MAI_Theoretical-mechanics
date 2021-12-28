2fba64537eae326e10c336b85d80e67ce93eb13e
Maxsmile123 <sysoevmaksim27@gmail.com>
2021-12-29 01:17:26 +0300

    add lab4

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -19,12 +19,18 @@ def odesys(y, t, fom1, fom2):
     dydt = [y3, y4, fom1(y1, y2, y3, y4), fom2(y1, y2, y3, y4)]
     return dydt
 
+def formY2(y, t, fOm):
+    y1,y2 = y
+    dydt = [y2,fOm(y1,y2)]
+    return dydt
+
+
 
 # параметры системы
 m1 = 5
 m2 = 1
 l0 = 5
-R = l0 * 0.5
+R = l0 / 2
 
 AX = 0
 AY = 0
@@ -34,16 +40,16 @@ CY = - R - Rc
 
 # коэффициенты
 g = 9.81
-alpha = 1
+alpha = 0.5
 
 # определяем t как символ
 t = sp.Symbol('t')
 
 # определяем обобщенные координаты
 phi = sp.Function('phi')(t)
-psi = sp.Function('psi')(t)
+psi = 0
 omega_phi = sp.Function('omega_phi')(t)
-omega_psi = sp.Function('omega_psi')(t)
+omega_psi = 0
 
 # строим уравнения Лагранжа
 # 1) определяем кинетическую энергию
@@ -71,49 +77,46 @@ L = TT - Pi
 
 # уравнения Лагранжа
 ur1 = sp.diff(sp.diff(L, omega_phi), t) - sp.diff(L, phi) - M
-ur2 = sp.diff(sp.diff(L, omega_psi), t) - sp.diff(L, psi)
-
+# ur2 = sp.diff(sp.diff(L, omega_psi), t) - sp.diff(L, psi)
 
 print(ur1)
-print(ur2)
 # выделяем вторые производные методом Крамера
 a11 = ur1.coeff(sp.diff(omega_phi, t), 1)
-a12 = ur1.coeff(sp.diff(omega_psi, t), 1)
-a21 = ur2.coeff(sp.diff(omega_phi, t), 1)
-a22 = ur2.coeff(sp.diff(omega_psi, t), 1)
-b1 = -(ur1.coeff(sp.diff(omega_phi, t), 0)).coeff(sp.diff(omega_psi, t), 0).subs([(sp.diff(phi, t), omega_phi), (sp.diff(psi, t), omega_psi)])
-b2 = -(ur2.coeff(sp.diff(omega_phi, t), 0)).coeff(sp.diff(omega_psi, t), 0).subs([(sp.diff(phi, t), omega_phi), (sp.diff(psi, t), omega_psi)])
+#a12 = ur1.coeff(sp.diff(omega_psi, t), 1)
+#a21 = ur2.coeff(sp.diff(omega_phi, t), 1)
+#a22 = ur2.coeff(sp.diff(omega_psi, t), 1)
+b1 = -ur1.coeff(sp.diff(omega_phi, t), 0).subs(sp.diff(phi, t), omega_phi)
+#b2 = -(ur2.coeff(sp.diff(omega_phi, t), 0)).coeff(sp.diff(omega_psi, t), 0).subs([(sp.diff(phi, t), omega_phi), (sp.diff(psi, t), omega_psi)])
 
-detA = a11 * a22 - a12 * a21
-detA1 = b1 * a22 - b2 * a12
-detA2 = a11 * b2 - b1 * a21
+print(a11)
+print(b1)
+#detA = a11 * a22 - a12 * a21
+#detA1 = b1 * a22 - b2 * a12
+#detA2 = a11 * b2 - b1 * a21
 
-DphiDt = detA1 / detA
-DpsiDt = detA2 / detA
+DphiDt = b1 / a11
+#DpsiDt = detA2 / detA
+
+print(DphiDt)
 
 countOfFrames = 500
 
 # создаем систему дифференциальных уравнений
 T = np.linspace(0, 12, countOfFrames)
-fom1 = sp.lambdify([phi, psi, omega_phi, omega_psi], DphiDt, "numpy")
-fom2 = sp.lambdify([phi, psi, omega_phi, omega_psi], DpsiDt, "numpy")
+fom1 = sp.lambdify([phi, omega_phi], DphiDt, "numpy")
+
 
 # начальные значения
-phi0 = 0
-psi0 = 0.03
-dphi0 = -0.01
-dpsi0 = -0.01
-y0 = [phi0, psi0, dphi0, dpsi0]
+y0 = [math.pi/6, 0]
 
 # получаем решение системы диффуров
 # ПРОИЗВОДИМ ЧИСЛЕННОЕ ИНТЕГРИРОВАНИЕ
-sol = odeint(odesys, y0, T, args=(fom1, fom2))
+sol = odeint(formY2, y0, T, args=(fom1,))
 
 #sol - our solution
 #sol[:,0] - phi
-#sol[:,1] - psi
-#sol[:,2] - dphi/dt
-#sol[:,3] - dpsi/dt
+#sol[:,1] - dphi/dt
+
 
 # построение графика и подграфика с выравниманием осей
 
@@ -123,9 +126,8 @@ ax = fig.add_subplot(1, 2, 1)
 ax.axis('equal')
 ax.set(xlim=[-10, 15], ylim=[-15, 10])
 phi = sol[:, 0]
-psi = sol[:, 1]
-Vphi = sol[:, 2]
-Vpsi = sol[:, 3]
+
+
 
 # статичные объекты
 Circ_A = ax.plot(*circle(R, AX, AY), 'black', linewidth=2)
@@ -134,8 +136,9 @@ Line_AC = ax.plot([AX, CX], [AY - R, CY + Rc], 'black', linewidth=1)
 
 # динамичные объекты
 Rad_A,= ax.plot([AX, -R * sp.sin(phi[0])], [AY, - R * sp.cos(phi[0])], 'black')
-Line_CB, = ax.plot([AX + l0 + Rc, AX + l0 + Rc + (l0-phi[0]*R) * sp.sin(psi[0])], [AY - R - Rc, AY - R - Rc - (l0-phi[0]*R) * sp.cos(psi[0])], 'black', linewidth=1)
-Circ_B, = ax.plot(AX + l0 + Rc + (l0-phi[0]*R) * sp.sin(psi[0]), AY - R - Rc - (l0-phi[0]*R) * sp.cos(psi[0]), marker='o', markersize=10, color='black')
+Line_CB, = ax.plot([AX + l0 + Rc, AX + l0 + Rc + (l0-phi[0]*R) * sp.sin(psi)], [AY - R - Rc, AY - R - Rc - (l0-phi[0]*R) * sp.cos(psi)], 'black', linewidth=1)
+Circ_B, = ax.plot(AX + l0 + Rc + (l0-phi[0]*R) * sp.sin(psi), AY - R - Rc - (l0-phi[0]*R) * sp.cos(psi), marker='o', markersize=10, color='black')
+
 
 # доп. графики
 ax1 = fig.add_subplot(4, 2, 2)
@@ -146,21 +149,9 @@ ax1.set_ylabel('phi(t)')
 
 ax2 = fig.add_subplot(4, 2, 4)
 ax2.plot(T, sol[:, 1])
-ax2.set_title('psi(t)')
+ax2.set_title('phi(t)\'')
 ax2.set_xlabel('t')
-ax2.set_ylabel('psi(t)')
-
-ax3 = fig.add_subplot(4, 2, 6)
-ax3.plot(T, sol[:, 2])
-ax3.set_title("phi(t)'")
-ax3.set_xlabel('t')
-ax3.set_ylabel("phi(t)'")
-
-ax4 = fig.add_subplot(4, 2, 8)
-ax4.plot(T, sol[:, 3])
-ax4.set_title("psi(t)'")
-ax4.set_xlabel('t')
-ax4.set_ylabel("psi(t)'")
+ax2.set_ylabel('phi(t)\'')
 
 # расстояние между графиками
 plt.subplots_adjust(wspace=0.2, hspace=0.8)
@@ -169,8 +160,8 @@ plt.subplots_adjust(wspace=0.2, hspace=0.8)
 
 def anima(i):
     Rad_A.set_data([AX, -R * sp.sin(1.5 * phi[i])], [AY, - R * sp.cos(1.5 * phi[i])])
-    Line_CB.set_data([AX + l0 + Rc, AX + l0 + Rc + (l0-phi[i]*R) * sp.sin(psi[i])], [AY - R - Rc, AY - R - Rc - (l0-phi[i]*R) * sp.cos(psi[i])])
-    Circ_B.set_data(AX + l0 + Rc + (l0-phi[i]*R) * sp.sin(psi[i]), AY - R - Rc - (l0-phi[i]*R) * sp.cos(psi[i]))
+    Line_CB.set_data([AX + l0 + Rc, AX + l0 + Rc + (l0-phi[i]*R) * sp.sin(psi)], [AY - R - Rc, AY - R - Rc - (l0-phi[i]*R) * sp.cos(psi)])
+    Circ_B.set_data(AX + l0 + Rc + (l0-phi[i]*R) * sp.sin(psi), AY - R - Rc - (l0-phi[i]*R) * sp.cos(psi))
     return Rad_A, Line_CB, Circ_B
 
 
